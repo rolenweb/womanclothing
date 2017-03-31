80140fa2cb3d0d673e9e2963f92d2326b6b1f50b
rolenweb <rolenweb@yandex.ru>
2017-03-31 10:37:29 +0300

    test

--- basic/views/site/contact.php
+++ basic/views/site/contact.php
@@ -3,7 +3,7 @@
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
 /* @var $model app\models\ContactForm */
-
+/*test*/
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 use yii\captcha\Captcha;
